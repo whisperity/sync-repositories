819ac41c84551707248ed975a784c8fbdee1e523
Whisperity <whisperity@gmail.com>
2019-07-12 17:47:24 +0200

    User should explicitly specify "finding" new repos

--- __main__.py
+++ __main__.py
@@ -27,10 +27,16 @@ if __name__ == '__main__':
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     ARGS.add_argument('root_folder',
-                      help="The root of the directory tree where update "
-                           "should be run.",
+                      help="""The root of the directory tree where update
+                           should be run.""",
                       nargs='?',
                       default=os.getcwd())
+    ARGS.add_argument('--allow-new-repositories', '-a',
+                      help="""Allow every found repository, even new ones, to
+                           be updated. By default, only repositories that has
+                           been configured at least once are updated again.
+                           """,
+                      action='store_true')
 
     argv = ARGS.parse_args()
     print(argv)
@@ -39,10 +45,12 @@ if __name__ == '__main__':
 
     print("Checking '%s' for repositories..." % argv.root_folder,
           file=sys.stderr)
-    repositories = list(repository.get_repositories(argv.root_folder))
+    repository_to_update_data = {}
 
     # Perform a check that every repository's authentication status is known.
-    for repo in repositories:
+    for repo in repository.get_repositories(argv.root_folder):
+        repo_data = list()
+
         for remote, url, parts in repo.get_remotes():
             if repo.get_authentication_method(remote) == 'keyring':
                 kr_creds = keyring.get_credentials(*parts)
@@ -50,15 +58,27 @@ if __name__ == '__main__':
                     print("The repository '%s' has a remote server it is "
                           "connected to, but the authentication details "
                           "for this server are not known!"
-                          % os.path.basename(repo.path))
+                          % repo.path)
+                    if argv.allow_new_repositories:
+                        kr.discuss_keyring_security()
+                        kr.ask_user_for_password(keyring, url, parts)
 
-                    kr.discuss_keyring_security()
+                        repo_data.append((remote, url, parts))
+                    else:
+                        print("... Skipping this repository and remote from "
+                              "upgrade.")
+                elif kr_creds is False or kr_creds:
+                    # Allow updating the repository if some credentials are
+                    # known - or the fact that no credentials are needed is
+                    # known.
+                    repo_data.append((remote, url, parts))
 
-                    kr.ask_user_for_password(keyring, url, parts)
+        repository_to_update_data[repo] = repo_data
 
+    # Update repositories that had been selected for actual update.
     print("Performing repository updates...")
-    for repo in repositories:
-        for remote, url, parts in repo.get_remotes():
+    for repo, data in repository_to_update_data.items():
+        for remote, url, parts in data:
             if repo.get_authentication_method(remote) == 'keyring':
                 kr_creds = keyring.get_credentials(*parts)
                 if kr_creds is False:
